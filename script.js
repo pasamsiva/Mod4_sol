482f4b486c75d05240a25bac63c97f8c891cc06c
Sai Siva Ram Pasam <106372472+pasamsiva@users.noreply.github.com>
2022-06-24 10:48:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
 
-    var names = ["Yaakov", "Jaya", "Java", "Ravi", "Gowri", "Shiva", "Teja", "Judge", "Alex", "Jimmy"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     
     for (var i = 0; i < names.length; i++) {
     
